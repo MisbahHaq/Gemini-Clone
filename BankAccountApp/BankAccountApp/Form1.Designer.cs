fd4dbe64f13ece82e209726109070a2870d8e935
Misbah Haque <misbahmuneer2003@gmail.com>
2026-03-17 14:32:12 +0500

    Add BankAccountApp Windows Forms project
    Add a new BankAccountApp WinForms project and solution. Introduces BankAccount model (deposit/withdraw logic and AML limit), Form1 UI and code (create account, deposit, withdraw, data grid) and Program.cs. Also includes project file (csproj), .resx/designer files and generated build outputs, obj/bin artifacts and Visual Studio workspace metadata.

---
+++ BankAccountApp/BankAccountApp/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace BankAccountApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            OwnerTxt = new TextBox();
+            AmountNum = new NumericUpDown();
+            BankAccountsGrid = new DataGridView();
+            DepositBtn = new Button();
+            WithdrawBtn = new Button();
+            CreateAccountBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)AmountNum).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)BankAccountsGrid).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(34, 32);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Owner:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(34, 424);
+            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Amount:";
+            // 
+            // OwnerTxt
+            // 
+            OwnerTxt.Location = new Point(125, 29);
+            OwnerTxt.Name = "OwnerTxt";
+            OwnerTxt.Size = new Size(224, 36);
+            OwnerTxt.TabIndex = 2;
+            // 
+            // AmountNum
+            // 
+            AmountNum.Location = new Point(125, 422);
+            AmountNum.Maximum = new decimal(new int[] { 999999999, 0, 0, 0 });
+            AmountNum.Minimum = new decimal(new int[] { 999999999, 0, 0, int.MinValue });
+            AmountNum.Name = "AmountNum";
+            AmountNum.Size = new Size(224, 36);
+            AmountNum.TabIndex = 3;
+            AmountNum.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // BankAccountsGrid
+            // 
+            BankAccountsGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            BankAccountsGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            BankAccountsGrid.Location = new Point(369, 29);
+            BankAccountsGrid.Name = "BankAccountsGrid";
+            BankAccountsGrid.Size = new Size(550, 323);
+            BankAccountsGrid.TabIndex = 4;
+            // 
+            // DepositBtn
+            // 
+            DepositBtn.Location = new Point(369, 424);
+            DepositBtn.Name = "DepositBtn";
+            DepositBtn.Size = new Size(138, 37);
+            DepositBtn.TabIndex = 5;
+            DepositBtn.Text = "Deposit";
+            DepositBtn.UseVisualStyleBackColor = true;
+            DepositBtn.Click += DepositBtn_Click;
+            // 
+            // WithdrawBtn
+            // 
+            WithdrawBtn.Location = new Point(781, 424);
+            WithdrawBtn.Name = "WithdrawBtn";
+            WithdrawBtn.Size = new Size(138, 37);
+            WithdrawBtn.TabIndex = 6;
+            WithdrawBtn.Text = "Withdraw";
+            WithdrawBtn.UseVisualStyleBackColor = true;
+            WithdrawBtn.Click += WithdrawBtn_Click;
+            // 
+            // CreateAccountBtn
+            // 
+            CreateAccountBtn.Font = new Font("Segoe UI", 12F);
+            CreateAccountBtn.Location = new Point(168, 71);
+            CreateAccountBtn.Name = "CreateAccountBtn";
+            CreateAccountBtn.Size = new Size(138, 37);
+            CreateAccountBtn.TabIndex = 7;
+            CreateAccountBtn.Text = "Create account";
+            CreateAccountBtn.UseVisualStyleBackColor = true;
+            CreateAccountBtn.Click += CreateAccountBtn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(931, 497);
+            Controls.Add(CreateAccountBtn);
+            Controls.Add(WithdrawBtn);
+            Controls.Add(DepositBtn);
+            Controls.Add(BankAccountsGrid);
+            Controls.Add(AmountNum);
+            Controls.Add(OwnerTxt);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 16F);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)AmountNum).EndInit();
+            ((System.ComponentModel.ISupportInitialize)BankAccountsGrid).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox OwnerTxt;
+        private NumericUpDown AmountNum;
+        private DataGridView BankAccountsGrid;
+        private Button DepositBtn;
+        private Button WithdrawBtn;
+        private Button CreateAccountBtn;
+    }
+}
