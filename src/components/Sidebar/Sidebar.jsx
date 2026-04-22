49310b68a9a4af1e8c53fdcd34250b8854135aea
Misbah Haque <misbahmuneer2003@gmail.com>
2026-04-22 11:59:23 +0500

    Update

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import React, { useState } from 'react'
 
 const Sidebar = () => {
+
+    const [extended, setExtended] = useState(false)
     return (
         <div className='sidebar'>
             <div className="top">
-                <img className='menu' src={assets.menu_icon} alt='' />
+                <img onClick={() => setExtended(prev => !prev)} className='menu' src={assets.menu_icon} alt='' />
                 <div className="new-chat">
                     <img src={assets.plus_icon} alt="" />
-                    <p>New Chat</p>
+                    {extended ? <p>New Chat</p> : null}
                 </div>
-                <div className='recent'>
+                {extended ? <div className='recent'>
                     <p className='recent-title'>
                         Recent
                     </p>
@@ -19,24 +21,21 @@ const Sidebar = () => {
                         <img src={assets.message_icon} alt='' />
                         <p>What is react ...</p>
                     </div>
-                </div>
+                </div> : null}
+
             </div>
             <div className="bottom">
                 <div className='bottom-item recent-entry'>
                     <img src={assets.question_icon} />
-                    <p>Help</p>
+                    {extended ? <p>Help</p> : null}
                 </div>
                 <div className='bottom-item recent-entry'>
                     <img src={assets.history_icon} />
-                    <p>Activity</p>
+                    {extended ? <p>Activity</p> : null}
                 </div>
                 <div className='bottom-item recent-entry'>
                     <img src={assets.setting_icon} />
-                    <p>Settings</p>
-                </div>
-                <div className='bottom-item recent-entry'>
-                    <img src={assets.question_icon} />
-                    <p>Help</p>
+                    {extended ? <p>Settings</p> : null}
                 </div>
             </div>
         </div>
