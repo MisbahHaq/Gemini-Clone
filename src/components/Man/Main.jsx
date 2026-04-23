1f8afac833d9e6cef48378d484e2d30decb41fa9
Misbah Haque <misbahmuneer2003@gmail.com>
2026-04-23 12:31:55 +0500

    Update

--- src/components/Man/Main.jsx
+++ src/components/Man/Main.jsx
@@ -4,10 +4,22 @@ import { assets } from '../../assets/assets'
 
 const Main = () => {
     return (
-        <div className='main'>
+        <div className="main">
             <div className='nav'>
                 <p>Gemini</p>
                 <img src={assets.user_icon} alt="" />
+            </div>
+            <div className='main-container'>
+                <div className='greet'>
+                    <p><span>Hello, Dev.</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className='cards'>
+                    <div className='card'>
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img src="" alt="" />
+                    </div>
+                </div>
             </div>
         </div>
     )
